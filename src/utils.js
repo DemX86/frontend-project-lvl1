ff4d4f33930d89ad766e64e627e6944695854125
Demin Dmitry <heaven7pro@gmail.com>
2022-01-16 21:46:37 +0300

    Fix bug with random array items

--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,6 @@ const getRandomInt = (min = 0, max = 100) => {
   return Math.floor(Math.random() * (mx - mn) + mn);
 };
 
-const getRandomArrayItem = (items) => items[getRandomInt(items.length)];
+const getRandomArrayItem = (items) => items[Math.floor(Math.random() * items.length)];
 
 export { getRandomInt, getRandomArrayItem };
