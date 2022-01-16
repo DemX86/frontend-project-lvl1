c750b612ecb4d71b16ef9c02f15376cc9afebf44
Demin Dmitry <heaven7pro@gmail.com>
2022-01-16 22:00:25 +0300

    Add guard expression to isPrime function

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,6 +2,9 @@ import { getRandomInt } from '../utils.js';
 import ROUNDS from '../const.js';
 
 const isPrime = (n) => {
+  if (n === 1) {
+    return false;
+  }
   for (let i = 2; i < n; i += 1) {
     if (n % i === 0) {
       return false;
