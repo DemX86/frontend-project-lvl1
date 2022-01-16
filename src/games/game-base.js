516eec61b3d8767a6cc259d917a33bc3f638b546
Demin Dmitry <heaven7pro@gmail.com>
2022-01-16 21:29:15 +0300

    Remove redundant check

--- src/games/game-base.js
+++ src/games/game-base.js
@@ -2,9 +2,6 @@ import readline from 'readline-sync';
 import ROUNDS from '../const.js';
 
 const runGame = (name, subject, questions, answers) => {
-  if (questions.length !== ROUNDS || answers.length !== ROUNDS) {
-    throw new Error(`Invalid arguments for runGame function: expected array of length ${ROUNDS}`);
-  }
   console.log(`${subject}\n`);
   for (let i = 0; i < ROUNDS; i += 1) {
     console.log(`Question ${i + 1}/${ROUNDS}: ${questions[i]}`);
