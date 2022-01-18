c7d32ba7c7ed609881cca3815357fc6f4c4d2053
Demin Dmitry <heaven7pro@gmail.com>
2022-01-18 18:21:56 +0300

    Replace GCD algorithm

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,38 +3,11 @@ import runGameEngine from '../game-base.js';
 
 const subject = 'Find the greatest common divisor of given numbers.';
 
-const getDivisors = (n) => {
-  const divisors = [1];
-  for (let i = 2; i <= n; i += 1) {
-    if (n % i === 0) {
-      divisors.push(i);
-    }
-  }
-  return divisors;
-};
-
 const getGCD = (a, b) => {
-  const divisorsA = getDivisors(a);
-  const divisorsB = getDivisors(b);
-
-  const common = [];
-  let i = 0;
-  let j = 0;
-  while (i < divisorsA.length && j < divisorsB.length) {
-    const divA = divisorsA[i];
-    const divB = divisorsB[j];
-    if (divA === divB && divA !== common[common.length - 1]) {
-      common.push(divA);
-      i += 1;
-      j += 1;
-    } else if (divA > divB) {
-      j += 1;
-    } else {
-      i += 1;
-    }
+  if (!b) {
+    return a;
   }
-
-  return Math.max(...common);
+  return getGCD(b, a % b);
 };
 
 const generateRound = () => {
